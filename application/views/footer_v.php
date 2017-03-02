837ae0b875c921710b3bec2ae7b461a05c0e491e
Logan Connor <loganconnor44@gmail.com>
2017-03-01 19:32:19 -0500

    Started the process of converting the ui to MaterializeCSS

---
+++ application/views/footer_v.php
@@ -0,0 +1,7 @@
+<?php
+	/**
+	 * Created by IntelliJ IDEA.
+	 * User: LoganConnor
+	 * Date: 2/27/17
+	 * Time: 6:56 PM
+	 */
--- application/views/stateLegislators_v.php
+++ application/views/stateLegislators_v.php
@@ -3,23 +3,163 @@
 <head>
 	<meta charset="utf-8">
 	<title>Politics Made Simple</title>
-	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700">
-	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendor/twbs/bootstrap/dist/css/bootstrap.min.css') ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap-material-design-master/dist/css/bootstrap-material-design.min.css') ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap-material-design-master/dist/css/ripples.min.css') ?>">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.js"></script>
-	<script src="<?php echo base_url('vendor/twbs/bootstrap/dist/js/bootstrap.js') ?>"></script>
-	<script src="<?php echo base_url('assets/bootstrap-material-design-master/dist/js/material.min.js') ?>"></script>
-	<script src="<?php echo base_url('assets/bootstrap-material-design-master/dist/js/ripples.min.js') ?>"></script>
+	<!--Import Google Icon Font-->
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css" media="screen,projection"/>
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<!--Import jQuery before materialize.js-->
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+	<!--Import Stately library-->
 	<link rel="stylesheet" href="<?php echo base_url('assets/stately-master/assets/css/stately.css') ?>">
+	<!--Import Charts.js library-->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.js"></script>
+	<!-- Import Custom CSS and JS files -->
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/custom/custom.css') ?>">
-	<script>
-		$(function() {
-			$.material.init();
-		});
-	</script>
+	<script src="<?php echo base_url('assets/custom/custom.js') ?>"></script>
+</head>
+<body>
+	<?php $this->load->view('navigation_v', $landingPage) ?>
+	<div class="teal white-text extraBottomPadding">
+		<div class="container">
+			<div class="row">
+				<div class="col s12 m12">
+					<h1 class="center-align"><?php echo $stateDetail->legislature_name ?></h1>
+					<div class="divider"></div>
+					<p>
+						<?php echo $stateDetail->legislature_name ?> members introduce bills, and approve or revoke the
+						legislation presented depending on it's affect on the population of the state.
+					</p>
+					<?php echo $htmlChamberResponse ?>
+					<p>
+						Their official webpage can be found <a href="<?php echo $stateDetail->legislature_url ?>">here</a>.
+					</p>
+					<h2>Events</h2>
+					<?php if(!$upcomingEvents) : ?>
+						<p>There are no upcoming events.</p>
+					<?php endif; ?>
+					<?php if(is_object($upcomingEvents)) : ?>
+						<p>There are upcoming events!</p>
+					<?php endif; ?>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12 m6">
+					<div class="card teal darken-2">
+						<div class="card-content white-text">
+							<span class="card-title"><h2 class="center-align">Party Distribution</h2></span>
+							<div class="divider"></div>
+							<canvas id="legisDonut"></canvas>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 m6">
+					<div class="card blue" id="testing">
+						<div class="card-content white-text">
+							<span class="card-title"><h2 class="center-align">House</h2></span>
+							<div class="divider"></div>
+							<p class="flow-text">
+								The House of Representatives is the larger of the two chambers and has the ability to initiate tax
+								legislation and the impeachment process of an official. The term length for a House Representative is
+								usually two years.
+							</p>
+							<p class="flow-text">
+								The leadership title within the House is called The Speaker of the House.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12 m6">
+					<div class="card blue">
+						<div class="card-image">
+							<ul class="stately" data-toggle="collapse" href="#byStateLegislators" aria-expanded="false" aria-controls="byBill">
+								<li data-state="al" class="al">A</li>
+								<li data-state="ak" class="ak">B</li>
+								<li data-state="ar" class="ar">C</li>
+								<li data-state="az" class="az">D</li>
+								<li data-state="ca" class="ca">E</li>
+								<li data-state="co" class="co">F</li>
+								<li data-state="ct" class="ct">G</li>
+								<li data-state="de" class="de">H</li>
+								<li data-state="dc" class="dc">I</li>
+								<li data-state="fl" class="fl">J</li>
+								<li data-state="ga" class="ga">K</li>
+								<li data-state="hi" class="hi">L</li>
+								<li data-state="id" class="id">M</li>
+								<li data-state="il" class="il">N</li>
+								<li data-state="in" class="in">O</li>
+								<li data-state="ia" class="ia">P</li>
+								<li data-state="ks" class="ks">Q</li>
+								<li data-state="ky" class="ky">R</li>
+								<li data-state="la" class="la">S</li>
+								<li data-state="me" class="me">T</li>
+								<li data-state="md" class="md">U</li>
+								<li data-state="ma" class="ma">V</li>
+								<li data-state="mi" class="mi">W</li>
+								<li data-state="mn" class="mn">X</li>
+								<li data-state="ms" class="ms">Y</li>
+								<li data-state="mo" class="mo">Z</li>
+								<li data-state="mt" class="mt">a</li>
+								<li data-state="ne" class="ne">b</li>
+								<li data-state="nv" class="nv">c</li>
+								<li data-state="nh" class="nh">d</li>
+								<li data-state="nj" class="nj">e</li>
+								<li data-state="nm" class="nm">f</li>
+								<li data-state="ny" class="ny">g</li>
+								<li data-state="nc" class="nc">h</li>
+								<li data-state="nd" class="nd">i</li>
+								<li data-state="oh" class="oh">j</li>
+								<li data-state="ok" class="ok">k</li>
+								<li data-state="or" class="or">l</li>
+								<li data-state="pa" class="pa">m</li>
+								<li data-state="ri" class="ri">n</li>
+								<li data-state="sc" class="sc">o</li>
+								<li data-state="sd" class="sd">p</li>
+								<li data-state="tn" class="tn">q</li>
+								<li data-state="tx" class="tx">r</li>
+								<li data-state="ut" class="ut">s</li>
+								<li data-state="va" class="va">t</li>
+								<li data-state="vt" class="vt">u</li>
+								<li data-state="wa" class="wa">v</li>
+								<li data-state="wv" class="wv">w</li>
+								<li data-state="wi" class="wi">x</li>
+								<li data-state="wy" class="wy">y</li>
+							</ul>
+							<span class="card-title" style="background:rgba(0,0,0,0.5); padding: 0 3px; border-radius: 1px">Search For Legislators</span>
+							<a class="btn-floating halfway-fab waves-effect waves-light teal" id="repurposeAnchor">
+								<i class="material-icons scale-transition scale-in" onClick="getStatesFromPHP()" id="stateSearchIcon">search</i>
+							</a>
+						</div>
+						<form method="post" id="<?php echo $methodNames['legislators'] ?>" action="<?php echo base_url(index_page() . 'Legislature') ?>">
+							<div id="appendSelectHere"></div>
+							<button hidden onclick="document.getElementById('legislators').submit()"></button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
 </head>
 <body>
 <?php $this->load->view('navigation_v', $landingPage) ?>
@@ -80,12 +220,11 @@
 							borderWidth: 1
 						}]
 					},
-					options:{
-						title: {
-							display: true,
-							text: '<?php echo $stateDetail->legislature_name ?>',
-							fontColor: '#000',
-							fontSize: 20
+					options: {
+						legend: {
+							labels: {
+								fontColor:"white"
+							}
 						}
 					}
 				});
